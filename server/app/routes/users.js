07aa176d4a112470b31b20a4818435b95cc92728
Yuriy Ivanyuk <ivanyyuk@gmail.com>
2016-07-25 16:46:17 -0400

    fixed some required routes

--- server/app/routes/users.js
+++ server/app/routes/users.js
@@ -1,4 +1,16 @@
 'use strict'
 
 var router = require('express').Router();
+var User = require('../../db/models/user');
 
+router.get('/', function (req, res, next) {
+  //check user priviledge
+
+  //if user isAdmin then return all users
+  User.findAll()
+  .then(function (usersArray){
+    res.send(usersArray)
+  })
+})
+
+module.exports = router;
