ea377f5b996a2e7f596a5e667d28837c62512095
Yuriy Ivanyuk <ivanyyuk@gmail.com>
2016-07-25 11:40:39 -0400

    added Product Model

--- server/db/index.js
+++ server/db/index.js
@@ -3,4 +3,5 @@ var db = require('./_db');
 module.exports = db;
 
 var User = require('./models/user');
+var Product = require('./models/product');
 
---
+++ server/db/models/products.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var Sequelize = require('sequelize');
+
+var db = require('../_db');
+
+module.exports = db.define('product', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false;
+    },
+    category: {
+        type: Sequelize.STRING
+    },
+    descrition: {
+        type: Sequelize.TEXT,
+        allowNull: false;
+    },
+    photoUrl: {
+        type: Sequelize.STRING
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false;
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false;
+    }
+});
