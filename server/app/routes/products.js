ff7bd4edd0de0375d074947e2b823382985f2abe
Julia Siwak <JuliaNYC@users.noreply.github.com>
2016-07-25 17:17:25 -0400

    two get routes

--- server/app/routes/products.js
+++ server/app/routes/products.js
@@ -1,10 +1,24 @@
 'use strict'
 
 var router = require('express').Router();
+var Products=require('../../db/models/product');
 
 router.get('/', function(req, res, next) {
-  res.send('request made')
+  Products.findAll()
+  .then(function(allProducts){
+  	res.send(allProducts);
+  })
+  .catch(next);
 });
+
+router.get('/:id', function(req,res,next){
+	var id=req.params.id;
+	Products.findById(id)
+	.then(function(oneProduct){
+		res.send(oneProduct);
+	})
+	.catch(next);
+})
 
 
 module.exports = router;
