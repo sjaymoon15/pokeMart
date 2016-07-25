b7439274824f83372f2c6d3b4f1ef1c3dbe930a0
Yuriy Ivanyuk <ivanyyuk@gmail.com>
2016-07-25 12:05:12 -0400

    few filename changes

--- server/db/index.js
+++ server/db/index.js
@@ -5,3 +5,4 @@ module.exports = db;
 var User = require('./models/user');
 var Product = require('./models/product');
 var Review = require('./models/review');
+var Order = require('./models/order')
