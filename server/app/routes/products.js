534cbae6006909f5b7d7135b0b6a7b085ee7d9a6
Julia Siwak <JuliaNYC@users.noreply.github.com>
2016-07-25 18:08:47 -0400

    all product routes

--- server/app/routes/products.js
+++ server/app/routes/products.js
@@ -11,6 +11,20 @@ router.get('/', function(req, res, next) {
   .catch(next);
 });
 
+router.get('/category/:category', function(req, res, next) {
+  Products.findAll({
+  	where:{
+  		category:req.params.category
+  	}
+  })
+
+  .then(function(foundProducts){
+  	res.send(foundProducts);
+  })
+  .catch(next);
+});
+
+
 router.get('/:id', function(req,res,next){
 	var id=req.params.id;
 	Products.findById(id)
@@ -20,5 +34,53 @@ router.get('/:id', function(req,res,next){
 	.catch(next);
 })
 
+router.post('/', function(req,res,next){
+	Products.create(req.body)
+	.then(function(newProduct){
+		res.status(201).send(newProduct);
+	})
+	.catch(next);
+})
+
+router.put('/:id', function(req,res,next){
+	var id=req.params.id;
+	Products.findById(id)
+	.then(function(product){
+		if (!product){
+			res.status(404).send();
+		}
+		else {
+			return product.update(req.body);
+		}
+	})
+	
+	.then(function(updatedProduct){
+		res.send(updatedProduct);
+	})
+	.catch(next);
+})
+
+router.delete('/:id', function(req,res,next){
+	var id=req.params.id;
+
+Products.findById(id)
+.then(function(product){
+	if (!product){
+		res.status(404).send();
+	}
+	else {
+		product.destroy({
+	where: {
+		id:req.params.id
+	} 
+})
+.then(function(){
+	res.status(204).end();
+})
+	}
+})
+
+  .catch(next);
+})
 
 module.exports = router;
