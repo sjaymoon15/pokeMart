8de9f94282a42d22dba3451a4cdac39c6cfeee99
Julia Siwak <JuliaNYC@users.noreply.github.com>
2016-07-29 13:04:33 -0400

    review routes

--- server/app/routes/reviews.js
+++ server/app/routes/reviews.js
@@ -3,6 +3,7 @@
 var router = require('express').Router();
 var db = require('../../db')
 var Review = db.model('review')
+var HttpError=require('../../utils/HttpError')
 
 // OB/SB: move this to /api/products/:productId/reviews
 router.get('/:productId', function(req,res,next){
@@ -22,5 +23,55 @@ router.post('/:productId', function(req,res,next){
     res.send(review)
   }).catch(next)
 })
+
+router.get('/:userId/reviews', function(req,res,next){
+Review.findAll({
+  where:{
+    authorId:req.params.userId
+  }
+})
+.then(function(reviews){
+  if (reviews){
+      res.send(reviews);
+  }
+  else {
+    res.status(204).send('You havent left any reviws yet')
+  }
+})
+  .catch(next);
+})
+
+
+router.put('/reviews/:id', function(req,res,next){
+  Reviews.findById(req.params.id)
+  .then(function(oneReview){
+    if (req.user.id===oneReview.authorId || req.user.isAdmin){
+      return oneReview.update(req.body);
+    }
+    else {
+      throw HttpError(401);
+    }
+  })
+  .then(function(updated){
+    res.status(200).send();
+  })
+    .catch(next);
+  })
+
+router.delete('/reviews/:id', function(req,res,next){
+  Review.findById(req.params.id)
+  .then(function(oneReview){
+    if (req.user.id===oneReview.authorId || req.user.isAdmin){
+      return oneReview.destroy();
+    }
+    else {
+      throw HttpError(401);
+    }
+  })
+  .then(function(){
+    res.status(204).send();
+  })
+  .catch(next);
+}) 
 
 module.exports = router
