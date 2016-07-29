4a0a4f9f6035bafffeef46c3ddcb099679d1fbac
yungtiec <tctammychu@gmail.com>
2016-07-29 18:31:45 -0400

    fix review form

--- browser/js/product/product.controller.js
+++ browser/js/product/product.controller.js
@@ -1,8 +1,10 @@
 app.controller('ProductCtrl', function ($scope, theProduct, allReviews, ProductFactory, CartFactory) {
     // product
+    $scope.newReview = {};
     $scope.product = theProduct;
     $scope.quantity = 1;
     $scope.reviews = allReviews;
+    console.log($scope);
     // review
     $scope.submitReview = function () {
         $scope.newReview.productId = $scope.product.id;
