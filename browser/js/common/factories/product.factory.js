1a31fc9390615cdd76d7b739648bf89cb1526f73
yungtiec <tctammychu@gmail.com>
2016-07-30 14:57:24 -0400

    add update/delete functionality

--- browser/js/common/factories/product.factory.js
+++ browser/js/common/factories/product.factory.js
@@ -1,5 +1,6 @@
 app.factory('ProductFactory', function ($http) {
 
+    var cachedProducts = [];
     var baseUrl = '/api/products/';
     var getData = res => res.data;
 
@@ -9,8 +10,36 @@ app.factory('ProductFactory', function ($http) {
         return $http.get(baseUrl).then(getData)
                 .then(function (products) {
                     return products.map(ProductFactory.convert);
+                }).then(function (products) {
+                    angular.copy(products, cachedProducts); // why angular copy alters array order!!!!!!!
+                    cachedProducts.sort(function (a, b) {
+                        return a.id - b.id;
+                    })
+                    return cachedProducts;
                 })
     };
+
+    ProductFactory.updateProduct = function (id, data) {
+        return $http.put(baseUrl + id, data)
+                .then(getData)
+                .then(ProductFactory.convert)
+                .then(function (product) {
+                    var updatedInd = cachedProducts.findIndex(function (product) {
+                        return product.id === id;
+                    });
+                    cachedProducts[updatedInd] = product;
+                    return product;
+                })
+    }
+
+    ProductFactory.deleteProduct = function (id) {
+        return $http.delete(baseUrl + id).success(function() {
+            var deletedInd = cachedProducts.findIndex(function (product) {
+                return product.id === id;
+            });
+            cachedProducts.splice(deletedInd, 1);
+        })
+    }
 
     ProductFactory.fetchById = function (id) {
         return $http.get(baseUrl + id)
