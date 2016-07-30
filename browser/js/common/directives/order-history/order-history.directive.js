4df0015c425e44b60a97cc59192ffae61c0a43eb
Seungjae Moon <sjaymoon15@gmail.com>
2016-07-30 19:42:38 -0400

    make order history state

---
+++ browser/js/common/directives/order-history/order-history.directive.js
@@ -0,0 +1,10 @@
+app.directive('orderHistory', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/common/directives/order-history/order-history.html',
+        scope: {
+            histories: '='
+        },
+        controller: 'OrderHistoryCtrl'
+    }
+})
