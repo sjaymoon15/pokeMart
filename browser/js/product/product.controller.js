429d7536df80f2a48d1f5db6059f7fa8ca412b54
yungtiec <tctammychu@gmail.com>
2016-08-02 08:42:04 -0400

    reset form

--- browser/js/product/product.controller.js
+++ browser/js/product/product.controller.js
@@ -8,6 +8,9 @@ app.controller('ProductCtrl', function ($scope, theProduct, allReviews, ProductF
     $scope.submitReview = function () {
         $scope.newReview.productId = $scope.product.id;
         ProductFactory.createReview($scope.product.id, $scope.newReview).then(function () {
+            console.log(ProductFactory.cachedReviews);
+            $scope.reviews = ProductFactory.cachedReviews;
+            $scope.newReview = {};
             Materialize.toast('Thank you!', 1000);
         }).catch(function () {
             Materialize.toast('Something went wrong', 1000);
