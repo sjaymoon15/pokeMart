b3d79fd9b27004c3140202936f2a148aaa534d5b
yungtiec <tctammychu@gmail.com>
2016-08-02 15:05:10 -0400

    add route for password reset

--- browser/js/cart/cart.controller.js
+++ browser/js/cart/cart.controller.js
@@ -1,21 +1,24 @@
  app.controller('CartCtrl', function($scope, $log, cartContent, CartFactory){
  	$scope.cartContent=cartContent;
- 	
+
  	$scope.remove= function(orderId) {
  		CartFactory.removeFromCart(orderId)
  		.then(function(newCart){
  			$scope.cartContent = newCart;
  		}).catch($log)
  	}
- 	
- 	$scope.changeQuantity=CartFactory.changeQuantity;
+
+ 	$scope.changeQuantity= function (cartId, quantity, addOrSubtract) {
+        CartFactory.changeQuantity(cartId, quantity, addOrSubtract);
+        $scope.cartContent = CartFactory.cachedCart;
+    };
 
   $scope.checkout = CartFactory.checkout;
 
   $scope.total = function() {
     var total = 0;
     cartContent.forEach(cart => total += (cart.price * cart.quantity))
-    
+
     return total;
   }
  })
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -47,6 +47,9 @@ password: {
 salt: {
     type: Sequelize.STRING
 },
+resetPasswordToken: {
+    type: Sequelize.STRING
+},
 twitter_id: {
     type: Sequelize.STRING
 },
@@ -62,6 +65,7 @@ google_id: {
             return _.omit(this.toJSON(), ['password', 'salt']);
         },
         correctPassword: function (candidatePassword) {
+            console.log('new encryptPassword',this.Model.encryptPassword(candidatePassword, this.salt))
             return this.Model.encryptPassword(candidatePassword, this.salt) === this.password;
         }
     },
@@ -74,10 +78,14 @@ google_id: {
             hash.update(plainText);
             hash.update(salt);
             return hash.digest('hex');
-        }
+        },
     },
     hooks: {
-        beforeValidate: function (user) {
+        beforeCreate: function (user) {
+            user.salt = user.Model.generateSalt();
+            user.password = user.Model.encryptPassword(user.password, user.salt);
+        },
+        beforeUpdate: function (user) {
             if (user.changed('password')) {
                 user.salt = user.Model.generateSalt();
                 user.password = user.Model.encryptPassword(user.password, user.salt);
