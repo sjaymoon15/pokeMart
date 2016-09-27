00861a7eb9cf39f2fdf6f04523a0ee5620922c23
Julia Siwak <JuliaNYC@users.noreply.github.com>
2016-09-27 14:45:46 -0400

    change design for payment page and addd edit button

--- browser/js/payment/payment.controller.js
+++ browser/js/payment/payment.controller.js
@@ -1,4 +1,4 @@
-app.controller('PaymentCtrl', function($scope,UserFactory, $log, CartFactory, totalCost, arrayOfItems){
+app.controller('PaymentCtrl', function($scope,UserFactory, $state, $log, CartFactory, totalCost, arrayOfItems){
   $scope.info = {};
   
   $scope.validateUser= function () {
@@ -8,6 +8,10 @@ app.controller('PaymentCtrl', function($scope,UserFactory, $log, CartFactory, to
           $scope.showCC = true;
         }).catch($log.error)
         
+  }
+  $scope.edit=function(){
+    $state.go('cart')
+
   }
   $scope.totalCost = totalCost;
   $scope.arrayOfItems = arrayOfItems;
