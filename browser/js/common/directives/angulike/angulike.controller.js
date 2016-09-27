ee218ee8d298b783bf84c1587f05cdec6198925d
Julia Siwak <JuliaNYC@users.noreply.github.com>
2016-09-27 13:31:54 -0400

    change url name etc for social media links

--- browser/js/common/directives/angulike/angulike.controller.js
+++ browser/js/common/directives/angulike/angulike.controller.js
@@ -2,9 +2,9 @@
   app.controller('angulikeCtrl', [
       '$scope', function ($scope) {
           $scope.myModel = {
-              Url: 'http://jasonwatmore.com/post/2014/08/01/AngularJS-directives-for-social-sharing-buttons-Facebook-Like-GooglePlus-Twitter-and-Pinterest.aspx',
-              Name: "AngularJS directives for social sharing buttons - Facebook, Google+, Twitter and Pinterest | Jason Watmore's Blog", 
-              ImageUrl: 'http://www.jasonwatmore.com/pics/jason.jpg'
+              Url: 'http://pokemart-fsa.herokuapp.com',
+              Name:  "Pokemart", 
+              ImageUrl: 'http://pokemart-fsa.herokuapp.com'
           };
       }
   ]);
