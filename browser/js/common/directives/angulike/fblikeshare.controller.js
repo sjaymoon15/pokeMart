7db0070e3c771f43ad87a1d5bb1e040542291be3
Julia Siwak <JuliaNYC@users.noreply.github.com>
2016-09-27 10:58:50 -0400

    trying to fix fb like things

---
+++ browser/js/common/directives/angulike/fblikeshare.controller.js
@@ -0,0 +1,9 @@
+app.controller('FBlike', [
+      '$scope', function ($scope) {
+          $scope.myModel = {
+              Url: 'http://pokemart-fsa.herokuapp.com',
+              Name: "Pokemart",
+              ImageUrl: 'http://pokemart-fsa.herokuapp.com'
+          };
+      }
+  ]);    
