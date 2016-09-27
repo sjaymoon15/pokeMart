66044b1ddc56fa958620963b9202fbd389fa36c6
Julia Siwak <JuliaNYC@users.noreply.github.com>
2016-09-27 00:15:12 -0400

    profile works AND finally adding to db too again

--- browser/js/common/factories/user.factory.js
+++ browser/js/common/factories/user.factory.js
@@ -1,3 +1,73 @@
+//the same as below but not working WTF!
+
+// app.factory('UserFactory', function ($http) {
+//     var UserFactory = {};
+
+//     var cachedUsers = [];
+//     var baseUrl = '/api/users/';
+//     var getData = res => res.data;
+
+//     UserFactory.fetchAll = function () {
+//         return $http.get(baseUrl).then(getData)
+//                 .then(function (users) {
+//                     angular.copy(users, cachedUsers); 
+//                     cachedUsers.sort(function (a, b) {
+//                         return a.id - b.id;
+//                     })
+//                     return cachedUsers;
+//                 })
+//     };
+
+
+//   UserFactory.fetchOne = function() {
+//     return $http.get(baseUrl + 'getLoggedInUserId')
+//             .then(getData)
+//   };
+
+
+//     UserFactory.updateUser = function (id, data) {
+//         return $http.get(baseUrl + id, data)
+//                 .then(getData)
+//                 .then(function (user) {
+//                     var updatedInd = cachedUsers.findIndex(function (user) {
+//                         return user.id === id;
+//                     });
+//                     cachedUsers[updatedInd] = user;
+//                     return user;
+//                 })
+//     }  
+
+//     UserFactory.deleteUser = function (id) {
+//         return $http.delete(baseUrl + id).success(function() {
+//             var deletedInd = cachedUsers.findIndex(function (user) {
+//                 return user.id === id;
+//             });
+//             cachedUsers.splice(deletedInd, 1);
+//         });
+//     }
+
+//     UserFactory.updateUserBeforePayment = function (infoObj){
+//         return $http.get(baseUrl + 'getLoggedInUserId')
+//             .then(getData)
+//             .then(function(user){
+//                 if(user.id === 'session'){
+//                 return $http.put('api/orders/cart/updateSessionCart', infoObj)
+//                 }
+//                 else{
+//                 return UserFactory.updateUser(user.id,infoObj)
+//                     .then(function () {
+//                         return $http.put('api/orders/cart/updateUserCart', infoObj)
+//                     })
+//                 }
+//             })
+//     }
+
+
+
+
+//     return UserFactory;
+// })
+
 app.factory('UserFactory', function ($http) {
     var UserFactory = {};
 
@@ -8,23 +78,20 @@ app.factory('UserFactory', function ($http) {
     UserFactory.fetchAll = function () {
         return $http.get(baseUrl).then(getData)
                 .then(function (users) {
-                    angular.copy(users, cachedUsers); 
+                    angular.copy(users, cachedUsers); // why angular copy alters array order!!!!!!!
                     cachedUsers.sort(function (a, b) {
                         return a.id - b.id;
                     })
                     return cachedUsers;
                 })
     };
-
-
-  UserFactory.fetchOne = function() {
+      UserFactory.fetchOne = function() {
     return $http.get(baseUrl + 'getLoggedInUserId')
             .then(getData)
   };
 
-
     UserFactory.updateUser = function (id, data) {
-        return $http.get(baseUrl + id, data)
+        return $http.put(baseUrl + id, data)
                 .then(getData)
                 .then(function (user) {
                     var updatedInd = cachedUsers.findIndex(function (user) {
@@ -33,7 +100,7 @@ app.factory('UserFactory', function ($http) {
                     cachedUsers[updatedInd] = user;
                     return user;
                 })
-    }  
+    }
 
     UserFactory.deleteUser = function (id) {
         return $http.delete(baseUrl + id).success(function() {
@@ -60,8 +127,6 @@ app.factory('UserFactory', function ($http) {
             })
     }
 
-
-
-
     return UserFactory;
 })
+
